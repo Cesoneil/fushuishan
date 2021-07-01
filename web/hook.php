0c13c3e3b00e98a2e633f6c7c984f703bb389951
cesoneil <cesoneil@sina.com>
2021-07-01 22:07:29 +0800

    test

--- web/hook.php
+++ web/hook.php
@@ -5,4 +5,4 @@ $client_token = $_GET['token'];
 if ($client_token !== $valid_token) die('Token mismatch!');
 $result = exec("cd /usr/develop/SuperMiners/; git pull origin master 2>&1",$output);
 echo '<pre>';
-var_dump($output); //这样可以用浏览器调试输出测试
+var_dump($output); //这样可以用浏览器调试输出
