1c3b0107d3c362e5b80f8f1c8c8e9e9412635561
cesoneil <cesoneil@sina.com>
2021-07-02 01:10:54 +0800

    add webhooks

--- web/hook.php
+++ web/hook.php
@@ -1,9 +1,5 @@
 <?php
 
-$result = exec("cd /usr/develop/fushuishan/&& sudo git stash && sudo git pull origin master 2>&1",$output,$status);
-echo '<pre>';
-var_dump($output); //这样可以用浏览器调试输出
-
 $valid_token = 'envl26tjbcnf234d3433m324m2234';
 
 //获取GitHub发送的内容
@@ -20,7 +16,9 @@ $payloadHash = hash_hmac($algo, $json, $valid_token);
 
 // 判断签名是否匹配
 if ($hash === $payloadHash) {
-    echo 签名正确;
+    $result = exec("cd /usr/develop/fushuishan/&& sudo git stash && sudo git pull origin master 2>&1",$output);
+    echo '<pre>';
+    var_dump($output); //这样可以用浏览器调试输出
 }else{
     $error  = 'Error: Token mismatch!'.PHP_EOL;
     $error .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
