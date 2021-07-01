7bf3ebc6141afbd5893133a341ad7c8c54c2a46a
cesoneil <cesoneil@sina.com>
2021-07-01 22:55:06 +0800

    add webhooks

--- web/hook.php
+++ web/hook.php
@@ -1,8 +1,8 @@
 <?php
 
 $valid_token = 'envl26tjbcnf234d3433m324m2234';
-$client_token = $_GET['token'];
+$client_token = $_GET['secret'];
 if ($client_token !== $valid_token) die('Token mismatch!');
-$result = exec("cd /usr/develop/SuperMiners/; git stash; git pull origin master 2>&1",$output);
+$result = exec("cd /usr/develop/SuperMiners/; git stash 2>&1; git pull origin master 2>&1",$output);
 echo '<pre>';
 var_dump($output); //这样可以用浏览器调试输出
