92e8ce61085fd77e4d97eecadac0807d78df96bf
cesoneil <cesoneil@sina.com>
2021-07-05 02:45:43 +0800

    test

--- web/hook.php
+++ web/hook.php
@@ -1,4 +1,8 @@
 <?php
+$result = exec("cd /usr/develop/fushuishan/ &&  git stash &&  git pull origin master 2>&1",$output);
+echo '<pre>';
+var_dump($output); //这样可以用浏览器调试输出
+exit;
 $valid_token = 'envl26tjbcnf234d3433m324m2234';
 
 //获取GitHub发送的内容
@@ -15,13 +19,14 @@ $payloadHash = hash_hmac($algo, $json, $valid_token);
 
 // 判断签名是否匹配
 if ($hash === $payloadHash) {
-//    $result = exec("cd /usr/develop/fushuishan/ &&  git stash &&  git pull origin master 2>&1",$output);
-//    echo '<pre>';
-//    var_dump($output); //这样可以用浏览器调试输出
-    echo 111;exit;
+    $result = exec("cd /usr/develop/fushuishan/ &&  git stash &&  git pull origin master 2>&1",$output);
+    echo '<pre>';
+    var_dump($output); //这样可以用浏览器调试输出
 }else{
     $error  = 'Error: Token mismatch!'.PHP_EOL;
-    $error .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
+    if(!empty($content)) {
+        $error .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
+    }
     $error .= '密钥不正确不能pull'.PHP_EOL;
     die($error);
 }
