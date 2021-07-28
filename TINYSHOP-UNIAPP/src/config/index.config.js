1286010b99ce3302be67ef490eb7bd8b5a6ae788
cesoneil <cesoneil@sina.com>
2021-07-29 02:05:05 +0800

    edit mall config

--- TINYSHOP-UNIAPP/src/config/index.config.js
+++ TINYSHOP-UNIAPP/src/config/index.config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     development: {
         assetsPath: '/static', // 静态资源路径
 				baseUrl: 'http://loc.fss.com/api', // 后台接口请求地址
-				hostUrl: 'http://localhost:1234', // H5地址(前端运行地址)
+				hostUrl: 'http://loc.fss.com:8090', // H5地址(前端运行地址)
         websocketUrl: '', // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
     },
     // 生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://demo.rageframe.com/api', // 后台接口请求地址
-        hostUrl: 'http://h5.tinyshop.rageframe.com', // H5地址(前端运行地址)
+        baseUrl: 'http://40675xg972.wicp.vip/api', // 后台接口请求地址
+        hostUrl: 'http://192.168.3.222:8090', // H5地址(前端运行地址)
         websocketUrl: '', // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
     }
