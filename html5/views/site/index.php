22bc8e3e020687dd63c81e36db3a9ae2df0de673
cesoneil <cesoneil@sina.com>
2021-08-11 03:09:42 +0800

    test

--- html5/views/site/index.php
+++ html5/views/site/index.php
@@ -3,19 +3,16 @@
 $this->title = '【限时免费体验】解决男性阳痿、早泄、勃起无力等问题，延长性爱时间、增大增粗请选择知缘堂';
 ?>
 
-<style>
-
-</style>
-
-<div class="flex-center position-ref full-height">
+<div class="position-ref full-height">
     <div class="content">
-        <div class="title m-b-md">
-            html5
-        </div>
+        <!--        <div class="title m-b-md">-->
+        <!--            html5-->
+        <!--        </div>-->
+        <a href="#form">
+            <img width="100%" class="user-image head_portrait" src="<?php echo \yii\helpers\Url::to('@web/resources/img/zhucedan_bg.jpg');?>"/>
+        </a>
 
-
-        <div class="links">
-<!--            <a href="#">欢迎进入手机应用</a>-->
+        <div id="form">
 
         </div>
     </div>
