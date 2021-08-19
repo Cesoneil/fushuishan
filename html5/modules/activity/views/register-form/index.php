92effc80979f93e6187211f3a83ec22cecd4063f
cesoneil <cesoneil@sina.com>
2021-08-19 23:09:34 +0800

    注册单页面完成

--- html5/modules/activity/views/register-form/index.php
+++ html5/modules/activity/views/register-form/index.php
@@ -5,8 +5,6 @@ $this->title = '【限时免费体验】解决 男性阳痿、早泄、勃起无
 
 use yii\bootstrap\ActiveForm;
 use html5\assets\AppAsset;
-
-
 AppAsset::register($this);
 ?>
 <style>
@@ -46,7 +44,7 @@ AppAsset::register($this);
         line-height: 50px;
         position: fixed;
         bottom: 0px;
-        z-index: 99;
+        z-index: 2;
         display: block;
     }
     .agree{  font-size: 12px;  }
@@ -54,10 +52,11 @@ AppAsset::register($this);
         width: 15%;  float: left;
     }
     .field-member-mobile{
-        width: 65%;  float: left;
+        width: 68%;  float: left;
     }
     .self-mobile {
-        width: 35%;  float: right;  line-height: 65px
+        width: 32%;  float: right;  line-height: 45px;
+        padding-top: 20px;
     }
 
     .box-header{
@@ -75,12 +74,22 @@ AppAsset::register($this);
     .auto-mobile{
         line-height: 36px;
     }
+    .call{
+        width: 50px;
+        height: 50px;
+        border-radius:50px ;
+        border:solid red 1px;
+        z-index:3;
+        position: fixed;
+        right: 5px;
+        bottom: 80px;
+    }
 
 </style>
 <div class="position-ref full-height">
     <div class="content">
         <a href="#form">
-            <img class="user-image head_portrait" src="<?php echo \yii\helpers\Url::to('@web/resources/img/head_img.jpg');?>"/>
+            <img class="user-image" src="<?php echo \yii\helpers\Url::to('@web/resources/img/head_img.jpg');?>"/>
         </a>
         <div class="box-header">
             <span class="header-left">
@@ -94,12 +103,12 @@ AppAsset::register($this);
         </div>
         <div id="form" class="box">
             <?php $form = ActiveForm::begin([
-                'id' => 'registerNK-form'
+                'id' => 'register-form'
             ]); ?>
             <div class="box-body">
                 <?= $form->field($model, 'realname')->textInput() ?>
                 <?= $form->field($model, 'mobile')->textInput() ?>
-                <div class="self-mobile"><span><i>o</i><span class="info-state">使用本机号码</s></span></div>
+                <div class="self-mobile"><span class="local-mobile"><i >o</i><span class="info-state">使用本机号码</s></span></div>
                 <span class="agree operator">使用本机号码即为同意
                     <span class="info-state">
                         <span class="move">《中国移动服务条款》</span>
@@ -117,7 +126,7 @@ AppAsset::register($this);
                 ]); ?>
                 <?= $form->field($model, 'address')->textarea() ?>
                 <?= $form->field($model, 'gender')->checkbox() ?>
-                <span class="agree auto-mobile" style="line-height: 36px">自动输入历史手机号 <span class="info-state">《个人信息授权与保护声明》</span></span>
+                <span class="agree auto-mobile">自动输入历史手机号<span class="info-state">《个人信息授权与保护声明》</span></span>
             </div>
             <div class="box-footer text-center">
                 <button style="background-color:#4e9fe4;width: 80%;padding: 40px auto;letter-spacing:5px;font-size: 20px;font-weight: 400" class="btn btn-primary" type="submit">立即提交</button>
@@ -125,12 +134,13 @@ AppAsset::register($this);
             <?php ActiveForm::end(); ?>
         </div>
         <a href="#form">
-            <img class="user-image head_portrait" src="<?php echo \yii\helpers\Url::to('@web/resources/img/zhucedan_bg.jpg');?>"/>
+            <img class="user-image" src="<?php echo \yii\helpers\Url::to('@web/resources/img/zhucedan_bg.jpg');?>"/>
         </a>
         <div class="support">优瑞建站仅向商家提供技术支持</div>
     </div>
-    <div class="agreement agree">提交即视为您已阅读并同意<span class="info-state"><<个人信息保护声明>></span></div>
+    <div class="agreement agree" hidden>提交即视为您已阅读并同意<span class="info-state"><<个人信息保护声明>></span></div>
     <div hidden>
-        成功弹窗，并且产生邀请码。深度挖掘
+         成功弹窗，并且产生邀请码。深度挖掘  这是发送验证码的页面
     </div>
+    <div class="call">电话</div>
 </div>
