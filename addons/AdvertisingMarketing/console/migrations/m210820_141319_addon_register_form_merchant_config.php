f60cb3a625d6d002ab4ad281ada4c4feb86206dd
cesoneil <cesoneil@sina.com>
2021-10-11 01:14:21 +0800

    修复建立数据库导致bug

--- addons/AdvertisingMarketing/console/migrations/m210820_141319_addon_register_form_merchant_config.php
+++ addons/AdvertisingMarketing/console/migrations/m210820_141319_addon_register_form_merchant_config.php
@@ -19,22 +19,22 @@ class m210820_141319_addon_register_form_merchant_config extends Migration
         /* 创建表 */
         $this->createTable('{{%addon_register_form_merchant_config}}', [
             'id' => "int(11) NOT NULL AUTO_INCREMENT",
-            'merchant_id' => "int(10) unsigned NULL DEFAULT '0' COMMENT '商户id'",
+            'merchant_id' => "int(11) unsigned NULL DEFAULT '0' COMMENT '商户id'",
             'merchant_name'=> "varchar(20) NULL DEFAULT '' COMMENT '商户展示名称'",
             'popularize_title' => "varchar(255) NOT NULL DEFAULT '' COMMENT '推广标题'",
             'popularize_img' => "varchar(255) NOT NULL DEFAULT '' COMMENT '推广图片'",
             'title' => "varchar(255) NOT NULL DEFAULT '' COMMENT '标题'",
-            'header_img' => "text NOT NULL DEFAULT '' COMMENT '头部图片'",//可以一张或者多张
+            'header_img' => "text NOT NULL COMMENT '头部图片'",//可以一张或者多张
             'header_img_show_mode' => "tinyint(1) unsigned NULL DEFAULT '1' COMMENT '头部图片展示方式[1:排列展示;2:轮播展示]'",
             'header_dynamic' => "tinyint(1) unsigned NULL DEFAULT '3' COMMENT '头部动态[0:不展示；1:只展示动态;2:只展示人数;3:全部展示]'",
 //           'default_agreement'=> "tinyint(1) unsigned NULL DEFAULT '1' COMMENT '头部动态[0:不使用；1:使用]'",
-            'agreement' => "text NULL DEFAULT ''  COMMENT '商家协议内容'",//如果为空使用默认协议
-            'footer_img' => "text NULL DEFAULT ''  COMMENT '尾部图片'",//可以0张或者多张
+            'agreement' => "text NULL  COMMENT '商家协议内容'",//如果为空使用默认协议
+            'footer_img' => "text NULL  COMMENT '尾部图片'",//可以0张或者多张
             'support_phone' => "varchar(20) NULL DEFAULT '' COMMENT '咨询电话'",//为空就不支持
             'register_number' => "int(11) NOT NULL DEFAULT '0' COMMENT '注册数量'",
             'click_number'  => "int(10) NULL DEFAULT '0' COMMENT '点击数'",
             'status' => "tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态(-1:已删除,0:禁用,1:正常)'",
-            'created_at' => "int(10) NULL DEFAULT '0' COMMENT '创建时间'",
+            'created_at' => "int(10) NOT NULL DEFAULT '0' COMMENT '创建时间'",
             'updated_at' => "int(10) unsigned NULL DEFAULT '0' COMMENT '修改时间'",
             'PRIMARY KEY (`id`)'
         ], "ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COMMENT='注册单_商户配置'");
